99d0291f00d51cbe9412b6e7ea66b89946a0e592
RicardoPayan <a218206581@unison.mx>
2022-08-13 20:36:53 -0700

    Logicamente hablando el segundo ejercicio funciona, pero la lista no se forma como deberia

--- tarea-01/tarea-01/problemas.rkt
+++ tarea-01/tarea-01/problemas.rkt
@@ -3,16 +3,27 @@
 ;; Escribe aquí tus soluciones
 
 ;;1
-;(define (countdown number)
-  ;(when (> number -1)
-    ;(printf "~a\n" (append (list) (list number)))
-    ;(countdown ( - number 1))
-    ;))
 (define (countdown number)
   (cond
     [(< number 0) null]
     [else (cons number (countdown (- number 1)))]))
 
+;;2
+
+
+(define (insertL primero segundo ls)
+
+  
+(cond
+    [(empty? ls) null ]
+    [(equal? (first ls) primero) (cons (cons segundo (first ls)) (insertL primero segundo (rest ls))) ]
+    [else (cons (first ls) (insertL primero segundo (rest ls)))]))
+   
+
+  ;(cond
+    ;[(empty? ls) null ]
+    ;[(equal? (first ls) primero) (const segundo(cons (first ls) (cons (insertL primero segundo (rest ls)) null)))]
+    ;[else (cons (first ls) (insertL primero segundo (rest ls)))]))
  
 
 
