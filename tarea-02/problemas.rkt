c85a4d318a5c85e9e32af4ee7b4e3f3508a950b9
RicardoPayan <a218206581@unison.mx>
2022-09-04 20:10:30 -0700

    Añadiendo firmas

--- tarea-02/problemas.rkt
+++ tarea-02/problemas.rkt
@@ -1,11 +1,12 @@
 #lang racket
 (require racket/trace)
 
-
+;unit-string?: string? -> boolean
 (define (unit-string? x)
   (and (string? x)
        (= (string-length x) 1)))
 
+;unit-string-list?: list? -> boolean
 (define (unit-string-list? x)
   (or (null? x)
       (and (pair? x)
@@ -13,23 +14,29 @@
            (= (string-length (first x)) 1)
            (unit-string-list? (rest x)))))
 
+;explode: string? -> list
 (define (explode s)
   (unless (string? s)
     (error 'explode "esperaba una cadena, pero recibí: ~e" s))
   (map string (string->list s)))
 
+;implode: list? -> string
 (define (implode ls)
   (unless (unit-string-list? ls)
     (error 'implode "esperaba una lista de cadenas unitarias, pero recibí: ~e" ls))
   (apply string-append ls))
 
 ;Problema 3
+
+;take: list?, integer? -> list
 (define (take ls n)
   (cond
     [(or (equal? n 0) (empty? ls)) null]
     [else (cons (first ls) (take (rest ls) (sub1 n)))]
   ))
 
+
+;drop: list?, integer? -> list
 (define (drop ls n)
   (cond
     [(or (equal? n 0) (empty? ls)) ls]
@@ -37,6 +44,7 @@
   ))
 
 
+;bundle: (list-of-symbol?), integer? -> list
 (define (bundle s n)
   (cond
     [(or (null? s) (or (zero? n) (< n 0))) s]
@@ -45,11 +53,14 @@
            (bundle (drop s n) n))]))
 
 ;Problema 6
+
+;list->chunks: list?, integer? -> list
 (define (list->chunks ls n)
   (cond
     [(empty? ls) null]
     [else (cons (take ls n) (list->chunks (drop ls n) n))]))
 
+;bundle-chunk: list?, integer? -> list
 (define (bundle-chunk s n)
  (cond
    [(or (null? s) (or (zero? n) (< n 0))) s]
@@ -59,19 +70,24 @@
              [else (cons (implode (first ls)) (add (rest ls)))]))]))
 
 ;Problema 7
+
+;partition: string?, integer? -> list
 (define (partition s n)
   (cond
     [(or (null? s) (or (zero? n) (< n 0))) s]
     [else (bundle (explode s) n)]))
 
+
 ;Problema 8
+
+;isort: list?, procedure? -> list
 (define (isort ls predicado)
   (if (empty? ls)
       null
       (insert (first ls)
               (isort (rest ls) predicado) predicado)))
  
-
+;;insert: integer?, list?, procedure? -> list
 (define (insert n ls predicado)
   (cond
     [(empty? ls) (list n)]
@@ -80,13 +96,14 @@
 
 ;Problema 10-15
 
-
+;same: list?, number? -> list
 (define (same ls pivot)
   (cond
     [(empty? ls) null]
     [(filter (equal? (first ls) pivot) ls) (cons (first ls) (same (rest ls) pivot))]
     [else (same (rest ls) pivot)]))
 
+;quicksort: list?, procedure? -> list
 (define (quicksort ls predicado)
   (unless (procedure? predicado) (error 'quicksort "Esperaba un predicado valido, recibi ~e" predicado))
   (cond
