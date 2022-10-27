3c6abf4142318f51d59f12a50cc0c3776e88b710
Durazo Duarte <a220216031@unison.mx>
2022-10-26 18:08:40 -0700

    valuesof

--- tarea-06/explicit-refs.rkt
+++ tarea-06/explicit-refs.rkt
@@ -2,15 +2,6 @@
 
 #|
 
- /*******  /*******   /******   /****** 
-| **__  **| **__  ** /**__  ** /**__  **
-| **  \ **| **  \ **| **  \ **| **  \__/
-| *******/| *******/| **  | **| **      
-| **____/ | **__  **| **  | **| **      
-| **      | **  \ **| **  | **| **    **
-| **      | **  | **|  ******/|  ******/
-|__/      |__/  |__/ \______/  \______/ 
-
 SINTAXIS CONCRETA
 =================
 
@@ -94,10 +85,13 @@ Expresión:
 (struct deref-exp (exp1)
   #:transparent)
 
-(struct setref-exp (exp1 exp2)
+(struct setref-exp (ref exp)
   #:transparent)
 
-(struct computar (referencia valor)
+(struct result (value state)
+  #:transparent)
+
+(struct letrec-exp (p-name b-var p-body letrec-body)
   #:transparent)
 
 (define (expression? x)
@@ -192,6 +186,25 @@ Expresión:
   (call-exp (parse-expression (first x))
             (parse-expression (second x))))
 
+
+(define (parse-newref x)
+  (unless (= (length x) 2)
+    (error 'parse "expresión no es válida: ~e" x))
+  (newref-exp (parse-expression (second x))))
+
+(define (parse-deref x)
+  (unless (= (length x) 2)
+    (error 'parse "expresión no es válida: ~e" x))
+  (deref-exp (parse-expression (second x))))
+
+(define (parse-setref x)
+  (unless (= (length x) 3)
+    (error 'parse "expresión no es válida: ~e" x))
+  ( setref-exp (parse-expression (second x))
+               (parse-expression (third x))))
+
+
+ 
 ;;;;;;;;;;;;;;
 ;; ENTORNOS ;;
 ;;;;;;;;;;;;;;
@@ -204,11 +217,17 @@ Expresión:
       (error 'environment "variable libre: ~e" var)
       (let ([binding (first env)])
         (if (equal? var (first binding))
-            (second binding)
+            (result (second binding) (get-store))
             (apply-env (rest env) var)))))
 
 (define (extend-env var val env)
   (cons (list var val) env))
+
+(define (extend-env-rec p-name b-var body saved-env)
+  (let ((vec (make-vector 1)))
+    (let ((new-env (extend-env p-name vec saved-env)))
+      (vector-set! vec 0 (proc-val (procedure b-var body new-env)))
+      new-env)))
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; PROCEDIMIENTOS ;;
@@ -217,13 +236,13 @@ Expresión:
 (struct procedure (var body saved-env)
   #:transparent)
 
-(define (apply-procedure proc val)
+(define (apply-procedure proc val st)
   (unless (procedure? proc)
     (error 'value-of "no es un procedimiento: ~e" proc))
   (let ([var (procedure-var proc)]
         [body (procedure-body proc)]
         [saved-env (procedure-saved-env proc)])
-    (value-of body (extend-env var val saved-env))))
+    (value-of body (extend-env var val saved-env) st)))
 
 ;;;;;;;;;;;;;;
 ;;   STORE  ;;
@@ -233,13 +252,14 @@ Expresión:
 
 (define the-store 'dummy)
 
-(define get-store the-store)
+(define (get-store) the-store)
 
-(define initialize-store!
+(define (initialize-store!)
   (set! the-store(empty-store)))
 
 (define (reference? v)
   (integer? v))
+
 
 
 ;;;;;;;;;;;;;;;
@@ -289,6 +309,17 @@ DenVal = Int + Bool + Proc
               (error type-name "no es una referencia ~e" ref))
             ref))
 
+(define expval->ref ref-val-ref)
+
+(struct pair-val (pair)
+  #:transparent
+  #:guard (lambda (pair type-name)
+            (unless (pair? pair)
+              (error type-name "no es un par ~e" pair))
+            pair))
+
+(define expval->pair pair-val-pair)
+
 #|
 
 ESPECIFICACIONES SEMÁNTICAS
@@ -329,49 +360,67 @@ ESPECIFICACIONES SEMÁNTICAS
   (cond
     [(const-exp? exp)
      (let ([n (const-exp-num exp)])
-       (computar (ref-val st) (num-val n)))]
+       (result (num-val n) st))]
     [(var-exp? exp)
-     (let ([var (var-exp-var exp)])
-       (apply-env env (computar (ref-val st) var)))]
+     (let ([var (var-exp-var exp)]) 
+       (apply-env env var))]
     [(diff-exp? exp)
      (let ([exp1 (diff-exp-exp1 exp)]
            [exp2 (diff-exp-exp2 exp)])
-       (num-val
-        (- (expval->num (value-of exp1 env))
-           (expval->num (value-of exp2 env)))))]
+       (let ([r1 (value-of exp1 env st)])
+         (let ([r2 (value-of exp2 env (result-state r1))])
+               (result (num-val (- (expval->num (result-value r1)) (expval->num (result-value r2))))
+                       (result-state r2)))))]
     [(zero?-exp? exp)
-     (let ([exp1 (zero?-exp-exp1 exp)])
-       (if (equal? 0 (expval->num (value-of exp1 env)))
-           (bool-val #t)
-           (bool-val #f)))]
+     (let* ([exp1 (zero?-exp-exp1 exp)]
+            [r1 (value-of exp1 env st)])
+       (if (equal? 0 (expval->num (result-value r1)))
+           (result (bool-val #t) st)
+           (result (bool-val #f) st)))]
     [(if-exp? exp)
      (let ([exp1 (if-exp-exp1 exp)]
            [exp2 (if-exp-exp2 exp)]
            [exp3 (if-exp-exp3 exp)])
-       (if (expval->bool (value-of exp1 env))
-           (value-of exp2 env)
-           (value-of exp3 env)))]
+       (let ([r1 (value-of exp1 env st)])
+         (if (expval->bool (result-value r1))
+           (value-of exp2 env (result-state r1))
+           (value-of exp3 env (result-state r1)))))]
     [(let-exp? exp)
      (let ([var (let-exp-var exp)]
            [exp1 (let-exp-exp1 exp)]
            [body (let-exp-body exp)])
-       (value-of body (extend-env var (value-of exp1 env) env)))]
+       (let ([r1 (value-of exp1 env st)])
+         (value-of body (extend-env var (result-value r1) env) (result-state r1))))]
     [(proc-exp? exp)
      (let ([var (proc-exp-var exp)]
            [body (proc-exp-body exp)])
-       (proc-val (procedure var body env)))]
+       (result (proc-val (procedure var body env)) st))]
     [(call-exp? exp)
-     (let ([rator (call-exp-rator exp)]
-           [rand (call-exp-rand exp)])
-       (let ([proc (expval->proc (value-of rator env))]
-             [arg (value-of rand env)])
-         (apply-procedure proc arg)))]
+     (let* ([rator (call-exp-rator exp)]
+           [rand (call-exp-rand exp)]
+           [r1 (value-of rator env st)]
+           [proc (expval->proc (result-value r1))]
+           [r2 (value-of rand env (result-state r1))]
+           [arg (result-value r2)])
+         (apply-procedure proc arg (result-state r2)))]
     [(newref-exp? exp)
-     (let* ([next-ref (length the-store)])
-       (set! the-store (append (the-store) (list (value-of exp env st))))
-       next-ref)]
+     (let ([this-ref (length st)]
+           [state (append st (list (result-value (value-of exp env st))))])
+       (result (ref-val this-ref) state))]
     [(deref-exp? exp)
-     (list-ref the-store (ref-val st))]
+     (if (null? st)
+         (error 'the-store "referencia sin contenido asignado: ~e" exp)
+         (result (list-ref exp st) st))]
+    [(setref-exp? exp)
+     (result setref-exp-exp (append (take st setref-exp-ref)
+                            (list (result-value (value-of setref-exp-exp env st)))
+                            (drop st (+ (expval->num setref-exp-ref) 1))))]
+    [(letrec-exp? exp)
+     (let* ([p-name (letrec-exp-p-body exp)]
+            [b-var (letrec-exp-b-var exp)]
+            [p-body (letrec-exp-p-body exp)]
+            [letrec-body (letrec-exp-letrec-body exp)])
+       (value-of letrec-body (extend-env-rec p-name b-var p-body env)))]
     [else
      (error 'value-of "no es una expresión: ~e" exp)]))
 
@@ -387,9 +436,10 @@ ESPECIFICACIONES SEMÁNTICAS
                (list 'h (num-val 6.62607015e-34)))))
 
 (define (value-of-program pgm)
+  (initialize-store!)
   (if (program? pgm)
       (let ([exp1 (a-program-exp1 pgm)])
-        (value-of exp1 (init-env)))
+          (value-of exp1 (init-env) the-store))
       (error 'value-of-program "no es un programa: ~e" pgm)))
 
 (define (run sexp)
